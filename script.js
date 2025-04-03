220ed38de2881a9932f8553eb666cf9b16ab1193
Julius <188955756+4483-s@users.noreply.github.com>
2025-04-03 16:12:32 +0800

    fix bugs and add new features

--- script.js
+++ script.js
@@ -1,40 +1,72 @@
 const container = document.querySelector(".container");
+const gridCount = 3;
+const gridSizeBtn = document.querySelector(".gridSize");
 let isDrawing = false;
-const gridCount = 16;
-for (i = 0; i < gridCount; i++) {
-  const childRow = document.createElement("div");
-  childRow.style.display = "flex";
-  childRow.style.gap = "0.2rem";
-  childRow.classList.add("row");
-  for (j = 0; j < gridCount; j++) {
-    const childSquare = document.createElement("div");
-    childSquare.style.backgroundColor = "white";
-    childSquare.classList.add("gr");
-    childSquare.style.width = "2rem";
-    childSquare.style.height = "2rem";
-    let isColored = false;
-    childSquare.addEventListener("mouseenter", function () {
-      if (!isDrawing && !isColored) {
-        childSquare.style.backgroundColor = "rgb(200, 210, 210)";
-      } else if (isDrawing) {
-        childSquare.style.backgroundColor = "red";
-        isColored = true;
-      }
-    });
-    childSquare.addEventListener("mousedown", () => {
-      isDrawing = true;
-      isColored = true;
-      childSquare.style.backgroundColor = "red";
-    });
-    childSquare.addEventListener("mouseleave", () => {
-      if (!isDrawing && !isColored) {
-        childSquare.style.backgroundColor = "white";
-      }
-    });
-    childSquare.addEventListener("mouseup", () => {
-      isDrawing = false;
-    });
-    childRow.append(childSquare);
-  }
-  container.append(childRow);
+document.addEventListener("dragstart", (e) => e.preventDefault());
+//button click
+function askForSize() {
+  const promptInputValue = window.prompt("grid size? must lower than 100");
+  return promptInputValue;
 }
+gridSizeBtn.addEventListener("click", () => {
+  let userGrid = 101;
+  while (userGrid > 100) {
+    userGrid = askForSize();
+    if (userGrid > 100) alert("to much");
+  }
+  if (userGrid) {
+    while (container.firstChild) {
+      console.log(container.firstChild);
+      container.removeChild(container.firstChild);
+    }
+
+    generateGrid(userGrid);
+  }
+});
+//
+
+document.addEventListener("mouseup", () => {
+  isDrawing = false;
+});
+container.addEventListener("mousedown", () => {
+  isDrawing = true;
+});
+function generateGrid(userGrid) {
+  for (i = 0; i < userGrid; i++) {
+    const childRow = document.createElement("div");
+    childRow.style.display = "flex";
+    childRow.style.flex = "1";
+    childRow.style.width = "100%";
+    for (j = 0; j < userGrid; j++) {
+      const childSquare = document.createElement("div");
+      childSquare.style.backgroundColor = "white";
+      childSquare.style.flex = 1;
+      let isColored = false;
+      childSquare.addEventListener("mouseenter", function () {
+        if (!isDrawing && !isColored) {
+          childSquare.style.backgroundColor = "rgb(200, 210, 210)";
+        }
+        if (isDrawing) {
+          childSquare.style.backgroundColor = "red";
+          isColored = true;
+        }
+      });
+      childSquare.addEventListener("mousedown", () => {
+        isDrawing = true;
+        isColored = true;
+        childSquare.style.backgroundColor = "red";
+      });
+      childSquare.addEventListener("mouseup", () => {
+        isDrawing = false;
+      });
+      childSquare.addEventListener("mouseleave", () => {
+        if (!isDrawing && !isColored) {
+          childSquare.style.backgroundColor = "white";
+        }
+      });
+      childRow.append(childSquare);
+    }
+    container.append(childRow);
+  }
+}
+generateGrid(gridCount);
